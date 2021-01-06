03bc35929ed11bd3d5f5ceb6e41393fe86180c06
raedaltaki <raed.simon@gmail.com>
2021-01-06 02:01:43 -0500

    add query parameter

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function()
+{
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) 
+    {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else 
+    {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) 
 {
@@ -22,7 +38,7 @@ var getRepoIssues = function(repo)
             }
             else
             {
-                alert("There was a problem with your request!");
+                document.location.replace("./index.html");
             }
         })
         .catch(function(error)
@@ -85,4 +101,4 @@ var displayWarning = function(repo)
     limitWarningEl.appendChild(linkEl);
 };
   
-getRepoIssues("facebook/react");
+getRepoName();
