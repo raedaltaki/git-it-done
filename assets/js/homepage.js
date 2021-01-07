12a11edcb1efbb944ef8c77b41b0eb26145591a8
raedaltaki <raed.simon@gmail.com>
2021-01-06 19:16:41 -0500

    add complex quires and language search buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) 
 {
@@ -80,6 +81,26 @@ var displayRepos = function(repos, searchTerm)
     }
 };
 
+var getFeaturedRepos = function(language) 
+{
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response)
+    {
+        if(response.ok)
+        {
+            response.json().then(function(data)
+            {
+                displayRepos(data.items, language);
+            });
+        }
+        else
+        {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
 var formSubmitHandler = function(event) 
 {
     event.preventDefault();
@@ -97,5 +118,17 @@ var formSubmitHandler = function(event)
     }
 };
 
+var buttonClickHandler = function(event)
+{
+    var language = event.target.getAttribute("data-language");
+    if (language) 
+    {
+        getFeaturedRepos(language);
+      
+        repoContainerEl.textContent = "";
+    }
+};
+
 
 userFormEl.addEventListener("submit", formSubmitHandler); 
+languageButtonsEl.addEventListener("click", buttonClickHandler);
